9c57a2a7831628080bf42bd2bf4eedb5c0b9b4c7
Sergio Cuervo Leon <scuervoleon@uniminuto.edu.co>
2019-09-28 15:06:50 -0500

    Formulario Estudiantes

--- server/routes.js
+++ server/routes.js
@@ -12,7 +12,6 @@ router.get("/usuario", (req, res) => {
   res.render('pages/usuario')
 })
 
-<<<<<<< HEAD
 router.get("/fAbogado", (req, res) => {
   res.render('pages/formularioAbogado')
 })
@@ -20,26 +19,9 @@ router.get("/fAbogado", (req, res) => {
 router.get("/abogado", (req, res) => {
   res.render('pages/vistaAbogado')
 })
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-router.get("/header", (req,res) => {
-  res.render('pages/partials/header')
+
+router.get("/fEstudiante", (req, res) => {
+  res.render('pages/formularioEstudiante')
 })
 
-=======
-<<<<<<< HEAD
->>>>>>> 597480993a8a129ac70827e11f85b304c4b4f421
-router.get("/abogado", (req, res) => {
-  res.render('pages/vistaAbogado')
-})
-
-=======
->>>>>>> b476b73db920470040d3ff776a2134d0cb67bcc1
-<<<<<<< HEAD
-=======
->>>>>>> bd2567a1f43dcd65ddec8226104f852b7f6a7ead
->>>>>>> 597480993a8a129ac70827e11f85b304c4b4f421
->>>>>>> 48199569f832b5a48a248eadb4459e06f36d3ac9
 module.exports = router;
