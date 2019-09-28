08c773f2f46f29c709d90c10fd39a1ff5a3d87df
Alejandro Zapata Molina <zapatamolinaalejandro@gmail.com>
2019-09-28 11:21:56 -0500

    Server

--- server/server.js
+++ server/server.js
@@ -8,3 +8,14 @@ const routes = require('./routes')
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
+// defincion de archivos publicos del servidor
+app.use(express.static('public'))
+// agregador de rutas al server
+app.use('/', routes)
+
+const port = process.env.PORT || 3000
+app.listen(port, (err) => {
+  if (err) throw err
+  console.log('server runnig  http://localhost:3000/')
+})
+
