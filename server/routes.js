650a2d68db051ad3b1bffa550392d204d2851884
Alejandro Zapata Molina <zapatamolinaalejandro@gmail.com>
2019-09-28 12:14:50 -0500

    Server

--- server/routes.js
+++ server/routes.js
@@ -2,7 +2,7 @@
 
 const express = require('express')
 const router = express.Router()
-const db = require('Bd')
+//const db = require('Bd')
 
 router.get('/', (req, res) => {
   res.render('server/views/pages/index.ejs')
